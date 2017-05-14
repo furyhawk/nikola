112cbbbee542608fc6f73ae05df57f5cab064efe
Olaf Meeuwissen <paddy-hack@member.fsf.org>
2017-05-14 21:16:04 +0900

    Add script to automate bumping the upstream version
    Idea courtesy of Sukil Etxenike, see #5.

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -eux
+# update.sh -- script to bump the upstream version
+# Copyright (C) 2017  Olaf Meeuwissen
+#
+# License: GPL-v3.0+
+
+version=$(docker run --rm $LAST_IMAGE nikola version --check \
+    | sed -n 's/.*Nikola==\([0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}\)`.*/\1/p')
+
+test x != x$version || exit 0
+
+sed -i "s/\(_VERSION: \"\)[^\"]*\(\"\)/\1$version\2/" .gitlab-ci.yml
+
+git commit -m "Bump upstream version" .gitlab-ci.yml
