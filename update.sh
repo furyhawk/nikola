eea2a454b4b803e65dc864844f9c5ae72771c051
Olaf Meeuwissen <paddy-hack@member.fsf.org>
2020-08-02 15:22:13 +0900

    Check validity of version string before commit.  Fixes #9

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -eu
 # update.sh -- script to bump the upstream version
-# Copyright (C) 2017  Olaf Meeuwissen
+# Copyright (C) 2017, 2020  Olaf Meeuwissen
 #
 # License: GPL-v3.0+
 
@@ -11,6 +11,12 @@ release=$(curl --silent \
 current=$(sed -n "s/^.*_VERSION: \"\([^\"]*\)\".*$/\1/p" .gitlab-ci.yml)
 
 test x$current != x$release || exit 0
+
+if test -z "$(echo $release | sed '/^[0-9][0-9]*\(\.[0-9][0-9]*\)*$/!d')"; then
+    echo "Unexpected version string: ($release)" >&2
+    echo "Please check" >&2
+    exit 1
+if
 
 if test -z "$(git status --porcelain -- .gitlab-ci.yml)"; then
     sed -i "s/\(_VERSION: \"\)[^\"]*\(\"\)/\1$release\2/" .gitlab-ci.yml
